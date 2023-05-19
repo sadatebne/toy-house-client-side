0c67565b78619743c19283a25ceb71a7cf2b9134
sadatebne <sadatshafin75@gmail.com>
2023-05-19 18:25:40 +0600

    register Page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import {
   } from "react-router-dom";
 import Main from "../components/Layouts/Main";
 import Login from "../components/Pages/Login/Login";
+import Register from "../components/Pages/Register/Register";
 
   const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ import Login from "../components/Pages/Login/Login";
         {
           path: "/login",
           element:<Login></Login> ,
+        },
+        {
+          path: "/register",
+          element:<Register></Register> ,
         },
         
       ],
