f49be1b47c5c50da5aa819d1641be267ff652269
sadatebne <sadatshafin75@gmail.com>
2023-05-20 14:22:28 +0600

    viewdetails Page updated

--- src/components/Pages/Category/ShowCategory.jsx
+++ src/components/Pages/Category/ShowCategory.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ShowCategory = ({category}) => {
     const {_id, name, photo, price, rating,} =category
     return (
-        <div >
+        <div className='my-10'>
             <div className="card w-96 h-full bg-base-100 shadow-xl mx-auto">
                 <figure className="px-10 pt-10">
                     <img src={photo} alt="Shoes" className="rounded-xl"  style={{ height: '300px', width: '300px' }} />
@@ -15,7 +15,7 @@ const ShowCategory = ({category}) => {
                     <div className="card-actions items-center">                        
                         <p>{rating}</p>
                         <Link to={`/viewToyDetails/${_id}`}>
-                        <button className="btn btn-primary">❯</button>
+                        <button className="btn btn-primary">View Details ❯</button>
                         </Link>
                     </div>
                 </div>
--- src/components/Pages/ViewToyDetails/ViewToyDetails.jsx
+++ src/components/Pages/ViewToyDetails/ViewToyDetails.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const ViewToyDetails = () => {
     const toys = useLoaderData()
-    const {photo}=toys
+    const {name, category,quantity, sellerName, photo, details, price, rating}=toys
     return (
         <div className='my-10'>
+            <Link to="/">Back To Main</Link>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
+                <div className="hero-content flex-col space-x-8 lg:flex-row ">
                     <img src={photo} className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                    <div className='space-y-6'>
+                        <h1 className="text-5xl font-bold">{name}</h1>
+                        <h3 className='text-2xl'>
+                            <span className='font-bold'>Category:</span> {category}
+                        </h3>
+                        <h3 className='text-2xl'>
+                            <span className='font-bold'>Seller-Name:</span> {sellerName}
+                        </h3>
+                        <p className="text-xl">{details}</p>
+                        <h3 className='text-2xl'>
+                            <span className='font-bold'>Price:</span> {price}
+                        </h3>
+                        <h3 className='text-2xl'>
+                            <span className='font-bold'>Available Quantity:</span> {quantity}
+                        </h3>
+                        <h3 className='text-2xl'>
+                            <span className='font-bold'>Rating:</span> {rating}
+                        </h3>
+                        <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
             </div>
